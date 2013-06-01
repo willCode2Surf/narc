7f17c62ffcd7333b4d4bd6c109ef848704f27758
lkptrzk <lkptrzk@gmail.com>
2013-06-01 12:52:15 -0400

    starting to form api

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # narc 
 
+```javascript
+    var narc = require('narc')
+
+    narc.setup('twilio.json');
+
+    narc.route('https://news.ycombinator.com', function ($) {
+      if 
+      
+
+    })
+
+    narc.scrape();
+
+```
--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 var jsdom = require('jsdom').jsdom
   , jquery = require('jquery')
   , request = require('request')
-  ;
 
-request('https://tito.io/nodeconf/nodeconf-2013', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+var routes = {
+  "https://news.ycombinator.com": function ($) {
+    console.log($('.title a').text())
+  }
+}
+
+var scrape_single = function (url, cb) {
+  request(url, function (error, response, body) {
+    if (error) {
+      throw error
+    } else if (response.statusCode !== 200) {
+      throw new Error('status not 200')
+    }
+    
     var window = jsdom(body).createWindow()
       , $ = jquery.create(window)
-    console.log($('.ticket-status-sold-out').text());
+
+    cb.call(response, $)
+  })
+}
+
+var scrape = function () {
+  for (var url in routes) {
+    if (routes.hasOwnProperty(url)) {
+      scrape_single(url, routes[url])
+    }
   }
-})
+}
+
+scrape()
+
